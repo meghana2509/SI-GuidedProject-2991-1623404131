ec7429c2cea18056705f196dc6061808d7020860
meghana2509 <85274981+meghana2509@users.noreply.github.com>
2021-07-08 16:28:37 +0530

    Update Iot project code.py

--- Iot project code.py
+++ Iot project code.py
@@ -19,10 +19,12 @@ client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
 client.connect()
 
 while True:
-    temp=random.randint(-20,125)
-    hum=random.randint(0,100)
+    temp=random.randint(-20,100)
     water=random.randint(0,100)
-    myData={'temperature':temp, 'humidity':hum, 'waterlevel':water}
+    Ph=random.randint(0,14)
+    Flouride=random.randint(0,1)
+    ISE=random.randint(0,10)
+    myData={'temperature':temp, 'waterlevel':water, 'Ph':Ph, 'flouride':Flouride, 'ISE':ISE}
     client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
     print("Published data Successfully: %s", myData)
     client.commandCallback = myCommandCallback
